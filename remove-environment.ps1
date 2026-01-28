ea24d8df33bb98eb264be2d6276ff508f435c3cb
Diogo Zanini <108477756+DiogoZanini@users.noreply.github.com>
2026-01-28 14:32:40 -0300

    Feat: Enhance remove-environment.ps1 for Docker cleanup
    Added commands to remove Docker images and prune unused images and volumes.

--- remove-environment.ps1
+++ remove-environment.ps1
@@ -1,2 +1,11 @@
 # Stop and remove docker and internal files
 docker rm -f sql_teste
+
+# Remove imagem do sql (opcional caso não vá usar)
+docker rmi mcr.microsoft.com/mssql/server:2022-latest
+
+# Remove imagens que não estão sendo usadas (opcional)
+docker image prune -f
+
+# limpa qualquer resíduo de banco de dados
+docker volume prune -f
